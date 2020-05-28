b9955fd1ea07cb2583758abf26522ab17c985d1d
Safiamoon <msellek.safia@gmail.com>
2020-05-28 22:57:34 +0200

    Contact page still in progress

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React, {Component}from 'react';
+import './Contact.scss'
+import { Avatar, AvatarBadge } from "@chakra-ui/core";
+
+const Contact = () => {
+    return(
+                <div className="contact">
+                    <h1>CONTACT US</h1>
+                </div>
+        
+        );
+};
+export default Contact;
