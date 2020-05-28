e0ddcb6824cd7bdcd0fe8b9f97aa482eaa7f2318
Safiamoon <msellek.safia@gmail.com>
2020-05-28 22:56:44 +0200

    Updated Goals + Header + Programs

--- src/Components/LandingPage/Header.js
+++ src/Components/LandingPage/Header.js
@@ -1,13 +1,15 @@
 import React, {Component}from 'react';
 import './Header.scss'
+import * as CONSTANTS from "../../constants";
+import {Link} from 'react-router-dom';
 
 const Header = () => {
     return(
             <div className="header row">
                     <div className="col-12 mt-5">
-                        <h1 className="text-center mt-5">YOUR STUDY PARTNER</h1>
-                        <p>Learn to code in Java, Symfony, React...</p>
-                        <a href="#" className="btn btn-md text-center">GET STARTED</a>
+                        <h1 className="text-center mt-5  animate__animated animate__heartBeat">YOUR STUDY PARTNER</h1>
+                        <p className=" animate__animated animate__heartBeat">Learn to code in Java, Symfony, React...</p>
+                        <Link to={CONSTANTS.LOGIN} className="btn btn-md text-center">GET STARTED</Link>
                     </div>
             </div>
         );
