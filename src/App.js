92d9c8f37f6fef7ce1ba5e9a2c4ffdc0f6b14394
Safiamoon <msellek.safia@gmail.com>
2020-05-28 13:53:10 +0200

    Login form

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import './App.scss';
 import Nav from "./Layout/Nav/Nav.js";
 import Footer from "./Layout/Footer/ConditionGeneral.js";
 import LandingPage from "./Components/LandingPage/LandingPage.js";
-import * as CONSTANTS from "./contants";
-import Login from "../Components/Login/Login.js";
-import History from "../Components/Utils/History.js";
+import * as CONSTANTS from "./constants";
+import Login from "./Components/Login/Login.js";
+import History from "./Components/Utils/History.js";
+import Header from "./Components/Header/Header.js";
 
 class App extends Component {
   render = () => {
     return (
       <>
-      <Nav logoTitle="WEBSTER"/>
+      
         <div>
           <Router history={History}>
-              <div className>
+              <Nav logoTitle="WEBSTER"/>
+              <div>
                 <Switch>
-                    <Route path={CONSTANTS.LANDINGPAGE} component ={LandingPage}/>
                     <Route path={CONSTANTS.LOGIN} component={Login}/>
+                    <Route path={CONSTANTS.LANDINGPAGE} component ={LandingPage}/>
                 </Switch>
               </div>
             <Footer />
