cfeadf34e6483317e0fb7b549badf3f0a2c21ec4
Safiamoon <msellek.safia@gmail.com>
2020-05-28 22:54:22 +0200

    Added new constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,6 @@
 export const LANDINGPAGE = "/";
 export const LOGIN ="/Login";
+export const CONTACT="/Contact";
+export const ABOUT="/About";
+export const PROGRAMMINGLANGUAGE= "/Programming-language";
+export const FRAMEWORK = "/Framework";
