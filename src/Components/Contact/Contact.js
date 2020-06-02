f2ab0ffbdd2005a39d39616b928f885028d46a87
Safiamoon <msellek.safia@gmail.com>
2020-06-02 11:06:24 +0200

    Added Contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,5 @@
 import React, {Component}from 'react';
 import './Contact.scss'
-import { Avatar, AvatarBadge } from "@chakra-ui/core";
 import Nav from '../../Layout/Nav/Nav.js';
 
 const Contact = () => {
@@ -9,6 +8,39 @@ const Contact = () => {
         <Nav />
                 <div className="contact">
                     <h1>CONTACT US</h1>
+                    <div class="container">
+                        <h4>We'd love to hear from you!</h4>
+                        
+                        <div class="row input-container">
+                                <div class="col-xs-12">
+                                    <div class="styled-input wide">
+                                        <input type="text" required />
+                                        <label>Name</label> 
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="styled-input">
+                                        <input type="text" required />
+                                        <label>Email</label> 
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+                                    <div class="styled-input" style= {{float:'right'}}>
+                                        <input type="text" required />
+                                        <label>Subject</label> 
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="styled-input wide">
+                                        <textarea required></textarea>
+                                        <label>Message</label>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="btn-lrg submit-btn">Send Message</div>
+                                </div>
+                        </div>
+                    </div>
                 </div>
         </>
         );
