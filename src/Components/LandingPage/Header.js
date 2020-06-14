e0c9993f585e7b2ef8a95205d41244bfe88fadfa
Safiamoon <msellek.safia@gmail.com>
2020-06-14 18:27:05 +0200

    Updated Header

--- src/Components/LandingPage/Header.js
+++ src/Components/LandingPage/Header.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Header.scss'
 import * as CONSTANTS from "../../Constants/constants";
 import {Link} from 'react-router-dom';
-
+import Nav from "../../Layout/Nav/Nav.js";
 const Header = () => {
     return(
+        <>
             <div className="header row">
                     <div className="col-12 mt-5">
                         <h1 className="text-center mt-5  animate__animated animate__heartBeat">YOUR STUDY PARTNER</h1>
@@ -12,6 +13,7 @@ const Header = () => {
                         <Link to={CONSTANTS.LOGIN} className="btn btn-md text-center">GET STARTED</Link>
                     </div>
             </div>
+            </>
         );
 };
 export default Header;
