f004c09be663593d7eb59b08ac5d20c5cc30bfe2
Safiamoon <msellek.safia@gmail.com>
2020-07-21 16:57:11 +0200

    Cleaning code

--- src/Components/Pages/ConnectedUserPage.js
+++ src/Components/Pages/ConnectedUserPage.js
@@ -12,6 +12,7 @@ const ConnectedUserPage = ()=>{
 
     return(
         <div> 
+            
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <img className= "pr-5" src={logo} alt="logo" width="15%"/>    
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -48,38 +49,6 @@ const ConnectedUserPage = ()=>{
             <h2 className="text-uppercase">Welcome back! We're glad your here.</h2>
             <p className="lead">You have recently consulted the followings programs : </p>
 
-            {/* <div className="card-deck">
-            <div className="card">
-                <img className="card-img-top" src="..." alt="Card image cap">
-                <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="..." alt="Card image cap">
-                <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p claclassNamess="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="..." alt="Card image cap">
-                <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                </div>
-                <div className="card-footer">
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </div>
-            </div>
-            </div> */}
         </div>
         
     );
