b60281336c8ddbd2bdda8d2552e33a598dedc2a5
Safiamoon <msellek.safia@gmail.com>
2020-07-21 17:01:12 +0200

    small modif

--- src/Components/Pages/ConnectedUserPage.js
+++ src/Components/Pages/ConnectedUserPage.js
@@ -12,7 +12,7 @@ const ConnectedUserPage = ()=>{
 
     return(
         <div> 
-            
+
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <img className= "pr-5" src={logo} alt="logo" width="15%"/>    
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,19 +23,8 @@ const ConnectedUserPage = ()=>{
                     <li className="nav-item active">
                         <Link className="text-dark font-weight-bold pr-5 nav-link" to={CONNECTED_USER}>Home</Link>
                     </li>
-                    <li className="nav-item dropdown pr-5 text-dark font-weight-bold">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Programs
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>JavaScript</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>Java</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>PHP</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>Symfony</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>Laravel</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>Spring</Link>
-                            <Link className="text-dark font-weight-bold nav-link" to={CONNECTED_USER}>React</Link>
-                        </div>
+                    <li className="nav-item active">
+                        <Link className="text-dark font-weight-bold pr-5 nav-link" to={CONNECTED_USER}>Programs</Link>
                     </li>
                     <li className="nav-item">
                         <Link className="text-dark font-weight-bold pr-5 nav-link" to={CONSTANTS.LOGIN}>Logout</Link>
