bc99fcd3b9d4d8ebefed783c20078a967ee50817
max <ren.maxime@gmail.com>
2020-07-24 09:47:55 +0200

    "#2 fixed betise de notre equipe : correction AddResource.js"

--- src/Components/AddResource/AddResource.js
+++ src/Components/AddResource/AddResource.js
@@ -10,7 +10,7 @@ const AddResource = (props) => {
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState(null);
     const token = localStorage.getItem('tokenUser');
-    const config = {headers: {Authorization:"Bearer " + token}};
+    const config = {headers: {Authorization:"Bearer " + token, 'Content-type': 'application/json'}};
 
     const axiosAddResource = (e) => {
     setIsLoading(true);
@@ -22,20 +22,22 @@ const AddResource = (props) => {
         level : e.target.elements.level.value,
         topic : e.target.elements.topic.value
     });
+    e.preventDefault();
 
-    addResource(data, config)
-        .then((res) => {
-            console.log(res);
-            history.push(CONSTANTS.PROGRAMS);
-        })
-        .catch((error) => {
-        console.log(error);
-        })
-        .finally(() => setIsLoading(false));
     };
 
     useEffect(() => {
         console.log(data)
+        addResource(data, config)
+            .then((res) => {
+                console.log(res);
+                history.push(CONSTANTS.PROGRAMS);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => setIsLoading(false));
+
     }, [data]);
 
 
