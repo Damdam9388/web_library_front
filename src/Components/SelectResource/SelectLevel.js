16ab049923c1879b95e292a7e110916c0cfbe6f8
Safiamoon <msellek.safia@gmail.com>
2020-08-01 22:38:37 +0200

    Cleaning code

--- src/Components/SelectResource/SelectLevel.js
+++ src/Components/SelectResource/SelectLevel.js
@@ -27,40 +27,6 @@ const SelectLevel = () => {
     );
 }
 
-// class SelectLevel extends Component {
-
-//     //State will apply to the po sts object 
-//     //We can pass data or functions down into components through props
-//     constructor(props){
-//         super(props);
-//         //Set initial state
-//         this.state={
-//             levels:[]
-//         };
-//     }
-
-//     componentDidMount() {
-//         //axios will perform a Http request to the api
-//         //Data is hosted in an endpoint
-//         axios.get(CONSTANTS.ENDPOINT_SELECT_LEVEL)
-//         //get the response and store the data
-//             .then(response => {
-//                 console.log(respone);
-//                 this.setState({levels: response.data});
-//             })
-//             .catch(error => console.log(error));
-//     }
-
-//     //render the data
-//     render(){
-//         return (
-//                 <div className="container">
-//                     {this.state.levels.map(level => (<ul key={level.id}>{level.name}</ul>))}
-//                 </div>
-//         )
-//     }
-// }
-
 
 export default SelectLevel;
 
--- src/Components/SelectResource/SelectTopic.js
+++ src/Components/SelectResource/SelectTopic.js
@@ -28,43 +28,5 @@ const SelectTopic = () => {
 }
 
 
-
-
-
-// class SelectTopic extends Component {
-
-//     //State will apply to the po sts object 
-//     //We can pass data or functions down into components through props
-//     constructor(props){
-//         super(props);
-//         //Set initial state
-//         this.state={
-//             topic:[]
-//         };
-//     }
-
-//     componentDidMount() {
-//         //axios will perform a Http request to the api
-//         //Data is hosted in an endpoint
-//         axios.get(CONSTANTS.ENDPOINT_SELECT_TOPIC)
-//         //get the response and store the data
-//             .then(response => {
-//                 console.log(respone);
-//                 this.setState({topics: response.data});
-//             })
-//             .catch(error => console.log(error));
-//     }
-
-//     //render the data
-//     render(){
-//         return (
-//                 <div className="container">
-//                     {this.state.topics.map(topic => (<ul key={topic.id}>{topic.discr}</ul>))}
-//                 </div>
-//         )
-//     }
-// }
-
-
 export default SelectTopic;
 
