4bccd749d5478a5cf0c55ceb144425f477e20228
max <ren.maxime@gmail.com>
2020-08-27 20:48:50 +0200

    Safiaisation des composants suivants : Framework.js FrameworkInfo.js Program.js ProgramInfo.js Programs.js

--- src/Components/FrameworkPage/Framework.js
+++ src/Components/FrameworkPage/Framework.js
@@ -1,7 +1,7 @@
 import React from "react";
 import * as CONSTANTS from "../../Constants/constants";
 import {Link} from "react-router-dom";
-
+//affiche les framework individuellement en recupérant le state frameworks de ProgramInfo.js
 const Framework = ({framework}) => {
 
     return (
@@ -9,10 +9,14 @@ const Framework = ({framework}) => {
             <div className="card-body">
                 <h5 className="card-title text-center">{framework.frameworkName}</h5>
                 <div className="col-md-12 d-flex flex-column justify-content-center align-items-center">
-                    <a href={framework.docUrl}  className="btn btn-primary">Link to the doc</a><br></br>
+                    <a href={framework.docUrl}  className="btn btn-primary">Link to the doc</a><br/>
+                    {/*
+                    on pointe a la manière program.js vers le composant frameworkInfo.js en passant
+                    l'id hydra dans l'url pour récupérer les ressources du framework
+                    */}
                     <Link className="btn btn-primary" to={`${CONSTANTS.FRAMEWORK_SINGLE}${framework['@id']}`}>
                         Go inside the framework
-                    </Link><br></br>
+                    </Link><br/>
                     <Link className="btn btn-primary" to={CONSTANTS.ADD_RESOURCE_FRAMEWORK}>Add resource</Link>
                 </div>
             </div>
