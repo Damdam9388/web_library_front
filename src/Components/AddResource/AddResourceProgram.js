c985bfcd2189a26cec746c9ddeb55e67945fd2fd
Safiamoon <msellek.safia@gmail.com>
2020-09-01 12:35:20 +0200

    Refacto : Authors, Levels and programs

--- src/Components/AddResource/AddResourceProgram.js
+++ src/Components/AddResource/AddResourceProgram.js
@@ -6,52 +6,34 @@ import {addResources} from "../../Services/AddResourceServices";
 
 const AddResourceProgram = (props) => {
     let history = useHistory();
+    const [authorInput, setAuthorInput] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState(null);
     const token = localStorage.getItem('tokenUser');
     const config = {headers: {Authorization:"Bearer " + token, 'Content-type': 'application/json'}};
 
 
-    //méthode utilisée si on crée un auteur en meme temps qu'on crée la ressource
-    const axiosAddResourceInput = (e) => {
-        setIsLoading(true);
-        setData({
-            name : e.target.elements.name.value,
-            url : e.target.elements.url.value,
-            //ici on crée un nouvel auteur, il faut donc créé un nouvel objet JSON, avec le champs name.
-            //c'est pourquoi name:e.target.elements.author.value est entre accolade
-            author : {name:e.target.elements.author.value},
-            language : e.target.elements.language.value,
-            level : e.target.elements.level.value,
-            topic : e.target.elements.program.value
-        });
-        e.preventDefault();
-    
-        };
-
-        // méthode utilisée si on selectionne un auteur existant lors de la création de la ressource
-        const axiosAddResource = (e) => {
-            setIsLoading(true);
-            setData({
-                name : e.target.elements.name.value,
-                url : e.target.elements.url.value,
-                author : e.target.elements.author.value,
-                language : e.target.elements.language.value,
-                level : e.target.elements.level.value,
-                topic : e.target.elements.program.value
-            });
-            e.preventDefault();
-            
-        };
-
-    useEffect(() => {
-        addResources(data, config, history, setIsLoading)
-    }, [data, config, history]);
+    // méthode utilisée si on crée un auteur en meme temps qu'on crée la ressource
+    const axiosAddResource = (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    let author = authorInput ? {name:e.target.elements.author.value} : e.target.elements.author.value;
+    const data = {
+        name : e.target.elements.name.value,
+        url : e.target.elements.url.value,
+        //ici on crée un nouvel auteur, il faut donc créé un nouvel objet JSON
+        //c'est pourquoi name:e.target.elements.author.value est entre accolade
+        author : author,
+        language : e.target.elements.language.value,
+        level : e.target.elements.level.value,
+        topic : e.target.elements.program.value
+    };
+        addResources(data, config, history, setIsLoading);
+    };
 
 
     return (
     <>
-        <AddResourceFormProgram getAddedResource={axiosAddResource} isLoading={isLoading} getAddedResourceInput={axiosAddResourceInput}/>
+        <AddResourceFormProgram getAddedResource={axiosAddResource} isLoading={isLoading} isInput={authorInput} setInput={setAuthorInput}/>
     </>
     );
 };
