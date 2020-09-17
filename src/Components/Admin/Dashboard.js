0010d74b516dd874fb77b28fc9b83ff3d6be31a0
max <ren.maxime@gmail.com>
2020-09-17 14:36:09 +0200

    admin icone button

--- src/Components/Admin/Dashboard.js
+++ src/Components/Admin/Dashboard.js
@@ -3,6 +3,8 @@ import ConnectedUserNav from "../../Layout/Nav/ConnectedUserNav";
 import UserContext from "../Context/UserContext";
 import {Link} from 'react-router-dom';
 import {ADMIN_PROGRAMS, ADMIN_RESOURCES, ADMIN_USERS} from "../../Constants/constants";
+import {Button} from "@chakra-ui/core";
+import {MdBuild , MdCall} from "react-icons/md";
 
 const Dashboard = () => {
 
@@ -13,10 +15,23 @@ const Dashboard = () => {
 
             <ConnectedUserNav username={username} />
             <h2 className="text-uppercase">Welcome back dear Admin !</h2>
-            <ul>
-                <li><Link to={ADMIN_USERS}>Users list</Link></li>
-                <li><Link to={ADMIN_PROGRAMS}>Programs list</Link></li>
-                <li><Link to={ADMIN_RESOURCES}>Resources list</Link></li>
+            <ul style={{listStyle: "none"}} className="mt-5 col-md-4 offset-4 d-flex flex-column justify-content-center align-items-center">
+                <li className="mt-3">
+                    <Button leftIcon={MdBuild} variantColor="blue" variant="outline">
+                    <Link to={ADMIN_USERS}>Users list</Link>
+                </Button>
+                </li>
+                <li className="mt-3">
+                    <Button leftIcon={MdBuild} variantColor="blue" variant="outline">
+                    <Link to={ADMIN_PROGRAMS}>Programs list</Link>
+                </Button>
+                </li>
+                <li className="mt-3">
+                    <Button leftIcon={MdBuild} variantColor="blue" variant="outline">
+                        <Link to={ADMIN_RESOURCES}>Resources list</Link>
+                    </Button>
+                </li>
+
             </ul>
 
         </div>
